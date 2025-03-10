c487e4a901d38d2961c3d09015739189eea8b1c4
Marcelo saad <marcelo_saad@hotmail.com>
2025-03-10 08:46:41 -0400

    fix: update

--- counter-service.py
+++ counter-service.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 # Define the path for the counter file to store the data in Docker Volume
 COUNTER_FILE = "/data/counter.txt"
 
+
 def read_counter():
     """
     Reads and returns the current counter value from the file.
     If the file doesn't exist, it return 0.
-    
+
     Returns:
         int: The current counter value.
     """
@@ -21,28 +22,30 @@ def read_counter():
     else:
         return 0
 
+
 def update_counter(counter):
     """
     Updates the counter file with the new counter value.
-    
+
     Args:
         counter (int): The new counter value to write to the file.
     """
     with open(COUNTER_FILE, "w") as f:
         f.write(str(counter))
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def handle_request():
     """
     Handles GET and POST requests to the root endpoint.
     GET request returns the current count of POST requests.
     POST request increments the counter and returns the updated count.
-    
+
     Returns:
         str: The response message with the current or updated counter.
     """
     counter = read_counter()
-    if request.method == 'POST':
+    if request.method == "POST":
         # Increment the counter for each POST request and update the file.
         counter += 1
         update_counter(counter)
@@ -51,12 +54,13 @@ def handle_request():
         # For GET requests, just return the current count.
         return f"Current POST requests count: {counter}"
 
-@app.route('/health', methods=['GET'])
+
+@app.route("/health", methods=["GET"])
 def health_check():
     """
     Performs a simple health check of the application.
     It tries to read the counter file as a basic check.
-    
+
     Returns:
         tuple: A JSON response indicating the health status and the HTTP status code.
     """
@@ -68,7 +72,8 @@ def health_check():
         # Return an unhealthy status if any error occurs, e.g., file access issues.
         return jsonify({"status": "unhealthy", "reason": str(e)}), 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Run the Flask app with binding to all interfaces on port 8080.
     # Debug mode is turned off for production use.
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=False)
